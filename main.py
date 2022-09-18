cc5ee941a8bccafd73c04a7bfd1c6b01543406f9
myrtyx <113023617+myrtyx@users.noreply.github.com>
2022-09-18 22:22:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,117 @@
+# Write code to accomplish each question as written in instructions.md.  Replace the empty string in each return statement with the correct return.
+
+#CHANGE THIS to do other tests!
+st = "ExampleString" 
+
+
+def question01(s):
+  s = st[2]
+  return s
+
+def question02(s):
+  s = st[5]
+  return s
+
+def question03(s):
+  num = len(st)
+  return num
+
+def question04(s):
+  s = st[0]
+  return s
+
+def question05(s):
+  s = st[-1]
+  return s
+
+def question06(s):
+  s = st[2:]
+  return s
+
+def question07(s):
+  s = st[3:8]
+  return s
+  
+def question08(s):
+  s = st[-5:]
+  return s
+  
+def question09(s):
+  s = st[3:]
+  return s
+
+def question10(s):
+  low = st.lower()
+  return low
+
+def question11(s):
+  upper = st.upper()
+  return upper
+
+def question12(s):
+  lst = list(st)
+  return lst
+
+def question13(s):
+  lst = list(st)
+  del(lst[0])
+  lst2 = "".join(lst)
+  return lst2
+
+def question14(s):
+  lst = list(st)
+  del(lst[-1])
+  lst2 = "".join(lst)
+  return lst2
+
+def question15(s):
+  return ""
+
+def question16(s):
+  return ""
+
+def question17(s):
+  return ""
+
+def question18(s):
+  return ""
+
+def question19(s):
+  return ""
+
+def question20(s):
+  return ""
+
+def question21(s):
+  return ""
+
+def question22(s):
+  return ""
+
+def question23(s):
+  return ""
+ 
+
+print("#1:", question01(st))
+print("#2:", question02(st))
+print("#3:", question03(st))
+print("#4:", question04(st))
+print("#5:", question05(st))
+print("#6:", question06(st))
+print("#7:", question07(st))
+print("#8:", question08(st))
+print("#9:", question09(st))
+print("#10:", question10(st))
+print("#11:", question11(st))
+print("#12:", question12(st))
+print("#13:", question13(st))
+print("#14:", question14(st))
+print("#15:", question15(st))
+print("#16:", question16(st))
+print("#17:", question17(st))
+print("#18:", question18(st))
+print("#19:", question19(st))
+print("#20:", question20(st))
+print("#21:", question21(st))
+print("#22:", question22(st))
+print("#23:", question23(st))
